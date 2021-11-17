f6f16b01ef3813d06ea479f89c8243379ede337b
Olle Brink Wennberg <olle.brinkwennberg@elev.ga.ntig.se>
2021-11-17 11:06:57 +0100

    .

--- Labyrint/Program.cs
+++ Labyrint/Program.cs
@@ -1,2 +1,31 @@
 ﻿using System;
 
+string room = "1";
+    
+Console.WriteLine("Hello and welcome to the maze!");
+Console.WriteLine("You are standing in room 1");
+Console.WriteLine("There is a door in front of you");
+
+while (room != "end")
+{
+    while (room == "1")
+    {
+        Console.WriteLine("Do you wish to enter?");
+        string choise = Console.ReadLine();
+        choise = choise.ToLower();
+      
+        if (choise == "yes")
+        {
+            Console.Clear();
+            room = "2";
+            Console.WriteLine("You are now in room 2");
+        }
+        if (choise == "no")
+        {
+            Console.WriteLine("");
+        }
+    }
+}
+
+
+Console.ReadLine();
